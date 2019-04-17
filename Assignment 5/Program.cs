aa89fd666e1d1786973c54fc3856e5a69e57c25e
visualstudio <visualstudio@122-PC37>
2019-04-17 10:28:59 -0400

    section-2

--- Assignment 5/Program.cs
+++ Assignment 5/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,52 @@ namespace Assignment_5
     {
         static void Main(string[] args)
         {
+            GenerateList a = new GenerateList();
+            a.Run(57);
+
+        }
+    }
+    class GenerateList
+    {
+        const int LEN = 100;
+
+        ArrayList ListQ;
+
+        public void Run(int AddUptoNumber)
+        {
+            Random r = new Random();
+
+            ListQ = new ArrayList();
+
+            for (int i = 0; i < LEN; i++)
+            {
+                ListQ.Add(r.Next(100));
+                this.findTwoNumbersThatAddUpTo(AddUptoNumber);
+                break;
+            }
+        }
+        public void findTwoNumbersThatAddUpTo(int a)
+        {
+            Random r = new Random();
+
+            ListQ = new ArrayList();
+            int n1 = 0;
+            int n2 = 0;
+            for (int i = 0; i < LEN; i++)
+            {
+                int num1 = ListQ.Add(r.Next(100));
+                
+                int num2 = ListQ.Add(r.Next(100));
+                if (num1 + num2 == a)
+                {
+                    n1 = num1;
+                    n2 = num2;
+                    Console.WriteLine($"The two numbers are {n1} and {n2}.");
+                }
+            }
         }
     }
 }
+        
+    
+
